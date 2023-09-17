2479f9f2de37915cc95d5f6c77a8cf91f04a9f2c
Eric Tossell <erictossell@gmail.com>
2023-09-17 10:16:38 -0400

    Restruct

--- configuration.nix
+++ configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, lib, pkgs, home-manager,  ... }:
+{ config, lib, pkgs, ... }:
 
 let
   corePackages = import ./packages/core.nix;
