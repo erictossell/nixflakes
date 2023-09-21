7afcc1e90e3f72c8eafbbf633f457a1a125b856e
Eric Tossell <erictossell@gmail.com>
2023-09-21 08:41:28 -0400

    Updated styling

--- modules/hyprland-nvidia.nix
+++ modules/hyprland-nvidia.nix
@@ -1,15 +1,20 @@
-{ pkgs, hyprland, ... }:
+{ pkgs, hyprland, home-manager, ... }:
 let
   hyprPackages = import ../packages/hyprland.nix { inherit pkgs; };
 in
-{
+{ 
+  home-manager.users.eriim = {
+    
+
+  };
+  
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   environment.systemPackages = with pkgs; [
     hyprland.packages.${system}.hyprland-nvidia
   ] ++ hyprPackages;
-  
+   
   programs.dconf.enable = true;
   services.gnome = {
     evolution-data-server.enable = true;
