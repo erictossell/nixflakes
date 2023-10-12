3a2f927f88d9502ce67e33546ac53cd2be2dfb1a
Eric Tossell <erictossell@gmail.com>
2023-10-12 09:13:25 -0400

    Fix black screen for apps on plasma with NIZ_OZONE

--- modules/plasma.nix
+++ modules/plasma.nix
@@ -6,6 +6,8 @@
 		displayManager.sddm.enable = true;
 		displayManager.defaultSession = "plasmawayland";
 		desktopManager.plasma5.enable = true;
+	environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
 		
   };
 }
