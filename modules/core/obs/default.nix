aad7dca02fcce300270f1179d756834ab32a941c
Eric Tossell <erictossell@gmail.com>
2023-10-18 09:06:49 -0400

    Refactor user, host > username, hostname

---
+++ modules/core/obs/default.nix
@@ -0,0 +1,23 @@
+{ pkgs, home-manager, username, ... }:
+{
+  home-manager.users.${username} = { pkgs, ... }: {
+
+     programs = {
+
+       obs-studio = {
+          enable = true;
+          plugins = with pkgs.obs-studio-plugins; [
+            wlrobs
+            obs-vaapi
+            obs-vkcapture
+            obs-pipewire-audio-capture
+          ];
+      };
+
+    };
+    services.easyeffects.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [ obs-studio];
+}
+
