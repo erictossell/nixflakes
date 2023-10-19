f5a3d8858ae0dde29527b1e85daf6c0a926e0002
Eric Tossell <erictossell@gmail.com>
2023-10-19 10:50:48 -0400

    Updated neovim

--- modules/core/terminal/dotfiles/nvim/init.lua
+++ modules/core/terminal/dotfiles/nvim/init.lua
@@ -401,6 +401,8 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+vim.keymap.set('n', '<leader>m', ':Dashboard<CR>', { noremap = true, silent = true })
+
 
 -- [[ Configure LSP ]]
 --  This function gets run when an LSP connects to a particular buffer.
--- modules/core/terminal/dotfiles/nvim/lua/custom/plugins/filetree.lua
+++ modules/core/terminal/dotfiles/nvim/lua/custom/plugins/filetree.lua
@@ -10,6 +10,10 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function ()
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+      close_if_last_window = false,
+      popup_border_style = "rounded",
+      enable_git_status = true,
+     }
   end,
 }
