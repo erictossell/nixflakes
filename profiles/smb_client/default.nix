1f315da9abbf0d8815e6c282e032888c59a95780
Eric Tossell <erictossell@gmail.com>
2023-10-22 23:44:21 -0400

    Updated smb-client

--- profiles/smb_client/default.nix
+++ profiles/smb_client/default.nix
@@ -15,8 +15,8 @@
   services.gvfs.enable = true;
   # environment.systemPackages = with pkgs; [ thunar ];
 
-  fileSystems."/home/${username}/shared" = {
-    device = "//nb/";
+  fileSystems."/home/${username}/Public" = {
+    device = "//nb/public";
     fsType = "cifs";
      options = let
         # this line prevents hanging on network split
@@ -24,5 +24,13 @@
 
       in ["${automount_opts},credentials=/home/${username}/.config/samba/smb-secrets"];
   };
+  fileSystems."/home/${username}/Private" = {
+    device = "//nb/private";
+    fsType = "cifs";
+     options = let
+        # this line prevents hanging on network split
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
+      in ["${automount_opts},credentials=/home/${username}/.config/samba/smb-secrets"];
+  };
 }
