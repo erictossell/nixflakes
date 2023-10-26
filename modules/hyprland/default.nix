8fd839597fba571afa88af10062801e712e7dc48
Eric Tossell <erictossell@gmail.com>
2023-10-26 14:21:32 -0400

    Greetd Module

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -9,11 +9,12 @@ in
   imports = [
     hyprland.nixosModules.default
     ./config
+    ./greetd
     ./mako
     ./swaylock
     ./waybar
     ./wofi
-  ]++ (hyprNvidia.${nvidia_bool} or [ ]);
+  ] ++ (hyprNvidia.${nvidia_bool} or [ ]);
   
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
@@ -29,28 +30,13 @@ in
     xdg-utils
   ];
   
-  programs.dconf.enable = true;
-
   programs.hyprland.enable = true;
-
-  services.greetd = {
-    enable = true;
-    restart = false;
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
-        user = username;
-      };
-      initial_session = {
-        command = "${pkgs.hyprland}/bin/Hyprland";
-        user = username;
-      };
-    };
-  };
-
+  programs.dconf.enable = true;
+ 
   services.gnome = {
     gnome-keyring.enable = true;
   };
+
   security = {
     pam = {
       services = {
