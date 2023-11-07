a500e7441cc0c91e197beffff28a1eb3b47ad1ef
Eric Tossell <erictossell@gmail.com>
2023-11-06 22:15:45 -0500

    Podman to virt

--- modules/virt/default.nix
+++ modules/virt/default.nix
@@ -11,11 +11,18 @@
 	allowedBridges = [ "virbr0" ];
   };
   
+  virtualisation.podman = {
+    enable = true;
+
+    defaultNetwork.settings.dns_enabled = true;
+
+  };
+
   # User permissions 
   users.users.${username}.extraGroups = [ "libvirtd" "docker" ];
 
   # Also recomended to install virt-manager in your packages if you want a GUI
-  environment.systemPackages = with pkgs; [ docker-compose virt-manager virt-viewer ];
+  environment.systemPackages = with pkgs; [ docker-compose podman-desktop podman-tui podman-compose virt-manager virt-viewer ];
   
   home-manager.users.${username} = { pkgs, ... }: {
    dconf.settings = {
