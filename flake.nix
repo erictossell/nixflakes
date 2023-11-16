440c8e1841e07c2124ef13136024104f364d9edc
Eric Tossell <erictossell@gmail.com>
2023-11-16 08:42:13 -0500

    Libvirt secure boot testing

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "Machine specific configuration flake.";
-  # Defining package channels
+  
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -15,39 +15,37 @@
 
   };
   
-  # Defining flake import structure for packages
+ 
   outputs = { self, nixpkgs, ... } @ attrs: { 
-    
-    # Hyprland Desktop - 3 monitors 
-    nixosConfigurations.retis = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = {
-        username = "eriim";
-        hostname = "retis";
-        displayConfig = "desktop";
-        nvidia_bool = "enabled";
-      } // attrs;        
-      modules = [
-            ./.
-            ./modules/obs
-            ./modules/toys
-            ./modules/virt
-          ];
+
+    nixosConfigurations = { 
+      retis = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {
+          username = "eriim";
+          hostname = "retis";
+          displayConfig = "desktop";
+          nvidia_bool = "enabled";
+        } // attrs;        
+        modules = [
+          ./.
+          ./modules/obs
+          ./modules/toys
+          ./modules/virt
+        ];
     };#retis
 
-    # Hyprland Laptop 
-    nixosConfigurations.sisyphus = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = {
-        username = "eriim";
-        hostname = "sisyphus";
-        displayConfig = "laptop";
-        nvidia_bool = "disabled";
-      } // attrs;        
-      modules = [
+      sisyphus = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {
+          username = "eriim";
+          hostname = "sisyphus";
+          displayConfig = "laptop";
+          nvidia_bool = "disabled";
+        } // attrs;        
+        modules = [
             ./.
-          ];
+        ];
     };#sisyphus
-
   };
 }
