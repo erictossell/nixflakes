116071a14fe9bf91061060429ca591010db7ed36
Eric Tossell <erictossell@gmail.com>
2023-11-16 12:29:26 -0500

    Modularization

--- hosts/default.nix
+++ hosts/default.nix
@@ -3,10 +3,21 @@
   imports = [
     ./${hostname}
   ];
+
   home-manager.users.${username} = { pkgs, ... }: {
     /* The home.stateVersion option does not have a default and must be set */
     home.stateVersion = "23.05";
     nixpkgs.config.allowUnfree = true;
   };
-  
+
+  # ----- System Config -----
+  # Enable Flakes and nix-commands
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+          
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+   
+  # Dont change.
+  system.stateVersion = "23.05"; # Did you read the comment?
+
 }
