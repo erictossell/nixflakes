a5eee6fef16ae11c3379b2305079dccc43286040
Eric Tossell <erictossell@gmail.com>
2023-12-07 22:52:17 -0500

    Tuigreet welcome message

--- modules/hyprland/greetd/default.nix
+++ modules/hyprland/greetd/default.nix
@@ -5,7 +5,7 @@
     restart = false;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --greeting 'Welcome to Wonderland, ${username}' --asterisks --cmd Hyprland";
         user = username;
       };
     };
