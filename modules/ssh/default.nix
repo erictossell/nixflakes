98c1f7f54c5b57bf4b530c01c5f5cc9b9ea8ba81
Eric Tossell <erictossell@gmail.com>
2023-12-08 17:47:06 -0500

    Principium ssh enabled

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -1,7 +1,7 @@
 { username, ... }:
 {
   users.users.${username} = {
-	openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAETSTzRnvYIQsOwhdwcbVRyZVnP6/F3b+inurb9+RMu ${user}" ];
+	openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAETSTzRnvYIQsOwhdwcbVRyZVnP6/F3b+inurb9+RMu ${username}" ];
   };
   
   services.openssh = {
