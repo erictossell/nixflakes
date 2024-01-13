1de22ba2ee88b63e59240a670ba25ba8a0ff36ad
Eric Tossell <erictossell@gmail.com>
2024-01-13 13:15:40 -0500

    Ambiens

---
+++ hosts/ambiens/default.nix
@@ -0,0 +1,19 @@
+{ nixpkgs, ... }:
+{
+  imports = [
+    (nixpkgs + "/nixos/modules/virtualisation/qemu-guest-agent.nix")
+    ./hardware-configuration.nix
+    ];
+  boot.loader = {
+    grub = { 
+      enable = true;
+      device = "/dev/vda";
+      useOSProber = true;
+      enableCryptodisk = true;
+      };
+      };
+      boot.initrd.secrets = {
+      "/crypto_keyfile.bin" = null;
+      };
+
+}
