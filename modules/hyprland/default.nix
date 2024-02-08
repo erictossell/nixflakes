3184811fd12f6eb8e79e8c317ab93ef17243066b
Eric Tossell <erictossell@gmail.com>
2024-02-07 19:41:00 -0500

    gtk dark theme - took me way too long to bother with this

--- modules/hyprland/default.nix
+++ modules/hyprland/default.nix
@@ -14,7 +14,7 @@
     gtk.enable = true;
     gtk.cursorTheme.name = "Adwaita";
     gtk.cursorTheme.package = pkgs.gnome.adwaita-icon-theme;
-    gtk.theme.name = "Adwaita-Dark";
+    gtk.theme.name = "adw-gtk3-dark";
     gtk.theme.package = pkgs.adw-gtk3;
   };
 
