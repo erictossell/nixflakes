6905b4d6021aef571eecb13f68d7e93ec568f15f
Eric Tossell <erictossell@gmail.com>
2024-03-13 12:33:30 -0400

    fmt

--- hosts/terminus/default.nix
+++ hosts/terminus/default.nix
@@ -1,4 +1,9 @@
 {
   imports = [ ./hardware-configuration.nix ];
   #----Host specific config ----
+  boot.kernelParams =
+    [ "i8042.direct" "i8042.dumbkbd" "i915.force_probe=9a49" ];
+  # ---- System Configurations ----
+  powerManagement.powertop.enable = true;
+  services.thermald.enable = true;
 }
--- sh/clean.nix
+++ sh/clean.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 pkgs.stdenv.mkDerivation {
   name = "clean";
@@ -15,4 +15,3 @@ pkgs.stdenv.mkDerivation {
   '';
 }
 
-  
