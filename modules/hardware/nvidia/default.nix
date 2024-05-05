71a1f9119e9138fed6e5936beecb7f3567b9e3eb
Eric Tossell <erictossell@gmail.com>
2024-05-05 00:27:15 -0400

    chore: flake update, nvidia stable

--- modules/hardware/nvidia/default.nix
+++ modules/hardware/nvidia/default.nix
@@ -1,6 +1,5 @@
 {
   config,
-  pkgs,
   username,
   ...
 }:
@@ -27,32 +26,8 @@
     powerManagement.finegrained = false;
     open = true;
     nvidiaSettings = true;
-    # Video card specific - stable supports "newer" cards, "production" is 550
     # https://nixos.wiki/wiki/Nvidia#Running_the_new_RTX_SUPER_on_nixos_stable
-    package =
-      let
-        rcu_patch = pkgs.fetchpatch {
-          url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
-          hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
-        };
-      in
-      config.boot.kernelPackages.nvidiaPackages.mkDriver {
-        version = "535.154.05";
-        sha256_64bit = "sha256-fpUGXKprgt6SYRDxSCemGXLrEsIA6GOinp+0eGbqqJg=";
-        sha256_aarch64 = "sha256-G0/GiObf/BZMkzzET8HQjdIcvCSqB1uhsinro2HLK9k=";
-        openSha256 = "sha256-wvRdHguGLxS0mR06P5Qi++pDJBCF8pJ8hr4T8O6TJIo=";
-        settingsSha256 = "sha256-9wqoDEWY4I7weWW05F4igj1Gj9wjHsREFMztfEmqm10=";
-        persistencedSha256 = "sha256-d0Q3Lk80JqkS1B54Mahu2yY/WocOqFFbZVBh+ToGhaE=";
-
-        #version = "550.40.07";
-        #sha256_64bit = "sha256-KYk2xye37v7ZW7h+uNJM/u8fNf7KyGTZjiaU03dJpK0=";
-        #sha256_aarch64 = "sha256-AV7KgRXYaQGBFl7zuRcfnTGr8rS5n13nGUIe3mJTXb4=";
-        #openSha256 = "sha256-mRUTEWVsbjq+psVe+kAT6MjyZuLkG2yRDxCMvDJRL1I=";
-        #settingsSha256 = "sha256-c30AQa4g4a1EHmaEu1yc05oqY01y+IusbBuq+P6rMCs=";
-        #persistencedSha256 = "sha256-11tLSY8uUIl4X/roNnxf5yS2PQvHvoNjnd2CB67e870=";
-
-        patches = [ rcu_patch ];
-      };
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 
   users.users.${username} = {
