c2166672efc876504e0a7364bd246e4b25da6bdc
Eric Tossell <erictossell@gmail.com>
2024-07-05 12:19:15 -0400

    fix: hypridle timeout

--- modules/hyprland/hypridle/default.nix
+++ modules/hyprland/hypridle/default.nix
@@ -14,18 +14,12 @@
         }
 
         listener {
-            timeout = 150                                # 2.5min.
-            on-timeout = brightnessctl -s set 10         # set monitor backlight to minimum, avoid 0 on OLED monitor.
-            on-resume = brightnessctl -r                 # monitor backlight restore.
-        }
-
-        listener {
-            timeout = 300                                 # 5min
+            timeout = 600                                 # 10min
             on-timeout = loginctl lock-session            # lock screen when timeout has passed
         }
 
         listener {
-            timeout = 330                                 # 5.5min
+            timeout = 630                                 # 10.5min
             on-timeout = hyprctl dispatch dpms off        # screen off when timeout has passed
             on-resume = hyprctl dispatch dpms on          # screen on when activity is detected after timeout has fired.
         }
