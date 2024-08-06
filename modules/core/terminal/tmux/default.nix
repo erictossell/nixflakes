3c69f8df4e6d7eed23959c55c58bf5bd1c6d2f95
Eric Tossell <erictossell@gmail.com>
2024-08-05 21:17:26 -0400

    feat: tmux gruvbox, hpyrland aperture, nix flake update

--- modules/core/terminal/tmux/default.nix
+++ modules/core/terminal/tmux/default.nix
@@ -1,11 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, username, ... }:
 {
-  # ---- System Configuration ----
-  programs = {
-    tmux = {
-      enable = true;
-      newSession = true;
-      plugins = with pkgs.tmuxPlugins; [ yank ];
+  home-manager.users.${username} = _: {
+    programs = {
+      tmux = {
+        enable = true;
+        newSession = true;
+        plugins = with pkgs.tmuxPlugins; [ 
+	  gruvbox
+	  yank 
+	];
+      };
     };
   };
 }
