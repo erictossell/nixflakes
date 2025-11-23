b98836e840f5edc7c18d99e82273993e179b3a72
Eric Tossell <erictossell@gmail.com>
2025-11-23 11:54:46 -0500

    chore: remove ovmf submodule - avaialble by default

--- modules/virt/libvirt.nix
+++ modules/virt/libvirt.nix
@@ -6,8 +6,8 @@
       qemu = {
         package = pkgs.qemu_kvm;
         swtpm.enable = true;
-        ovmf.enable = true;
-        ovmf.packages = [ pkgs.OVMFFull.fd ];
+        #ovmf.enable = true;
+        #ovmf.packages = [ pkgs.OVMFFull.fd ];
       };
     };
     spiceUSBRedirection.enable = true;
